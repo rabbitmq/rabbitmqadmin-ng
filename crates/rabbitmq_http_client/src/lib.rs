e795823d0792c0bc5018357d90591a9b96682f99
Michael Klishin <michael@clojurewerkz.org>
2023-05-21 20:25:11 +0400

    Continue sketching CLI args with Clap builder API

---
+++ crates/rabbitmq_http_client/src/lib.rs
@@ -0,0 +1,39 @@
+mod requests;
+mod responses;
+
+use reqwest::RequestBuilder;
+
+struct Client<'a> {
+    endpoint: &'a str,
+    username: &'a str,
+    password: Option<&'a str>
+}
+
+impl<'a> Client<'a> {
+    pub fn new_with_basic_auth_credentials(endpoint: &'a str, username: &'a str, password: Option<&'a str>) -> Self {
+        Self {
+            endpoint: endpoint,
+            username: username,
+            password: password
+        }
+    }
+
+    pub async fn get_node_info(&self, name: &str) -> responses::Result<responses::ClusterNode> {
+        let response = reqwest::Client::new()
+            .get(self.rooted_path(&format!("/nodes/{}", name)))
+            .basic_auth(self.username, self.password)
+            .send().await?;
+
+        println!("response: {:?}", response);
+        let node = response.json::<responses::ClusterNode>().await?;
+        Ok(node)
+    }
+
+    //
+    // Implementation
+    //
+
+    fn rooted_path(&self, path: &str) -> String {
+        return format!("{}/{}", self.endpoint, path)
+    }
+}
--- crates/rabbitmqadmin_cli/src/main.rs
+++ crates/rabbitmqadmin_cli/src/main.rs
@@ -1,10 +1,21 @@
-use clap::{Arg, Command};
+mod commands;
+
+use clap::{Arg, Command, ArgMatches};
 
 const DEFAULT_HOST: &str = "localhost";
 const DEFAULT_PORT: &str = "15672";
+const DEFAULT_PATH_PREFIX: &str = "/api";
+const DEFAULT_VHOST: &str = "/";
 
-fn list_subcommands() -> [Command; 3] {
+const DEFAULT_USERNAME: &str = "guest";
+const DEFAULT_PASSWORD: &str = "guest";
+
+// default node section in the configuration file
+const DEFAULT_NODE_ALIAS: &str = "default";
+
+fn list_subcommands() -> [Command; 4] {
     [
+        Command::new("nodes"),
         Command::new("users"),
         Command::new("vhosts"),
         Command::new("permissions")
@@ -43,9 +54,28 @@ fn main() {
         .version("0.1.0")
         .author("Michael Klishin")
         .about("rabbitmqadmin v2")
-        .arg(Arg::new("node").short('n').required(false))
-        .arg(Arg::new("host").short('H').required(false).default_value(DEFAULT_HOST))
-        .arg(Arg::new("port").short('P').required(false).value_parser(clap::value_parser!(u16)).default_value(DEFAULT_PORT))
+        .disable_version_flag(true)
+        // --node
+        // This is NOT the same as --node in case of rabbitmqctl, rabbitmq-diagnostics, etc.
+        // This is node section name in the configuration file. MK.
+        .arg(Arg::new("node").short('N').long("node").required(false).default_value(DEFAULT_NODE_ALIAS))
+        // --host
+        .arg(Arg::new("host").short('H').long("host").required(false).default_value(DEFAULT_HOST)).visible_alias("hostname")
+        // --port
+        .arg(Arg::new("port").short('P').long("port").required(false).value_parser(clap::value_parser!(u16)).default_value(DEFAULT_PORT))
+        // --base-uri
+        .arg(Arg::new("base_uri").short('U').long("base-uri").required(false))
+        // --path-prefix
+        .arg(Arg::new("path_prefix").long("path-prefix").required(false).default_value(DEFAULT_PATH_PREFIX))
+        // --vhost
+        .arg(Arg::new("vhost").short('V').long("vhost").required(false).default_value(DEFAULT_VHOST))
+        // --username
+        .arg(Arg::new("username").short('u').long("username").required(false).default_value(DEFAULT_USERNAME))
+        // --password
+        .arg(Arg::new("password").short('p').long("password").required(false).default_value(DEFAULT_PASSWORD).requires("username"))
+        // --quiet
+        .arg(Arg::new("quiet").short('q').long("quiet").required(false)
+                .value_parser(clap::value_parser!(bool)).action(clap::ArgAction::SetTrue))
         .subcommand_required(true)
         .subcommand_value_name("command")
         .subcommands([
@@ -68,5 +98,40 @@ fn main() {
         ])
         .get_matches();
     
-    println!("cli: {:?}", &cli);
+    if let Some((verb, group_args)) = cli.subcommand() {
+        if let Some((kind, _command_args)) = group_args.subcommand() {
+            let pair = (verb, kind);
+            println!("{:?}", pair);
+
+            let _cmd = match pair {
+                ("list", "nodes") => list_nodes_command(&cli),
+                ("list", "") => list_users_command(&cli),
+                _ => ()
+            };
+
+            // cmd.execute();
+        }
+    }
+}
+
+struct SharedFlags {
+    pub hostname: String,
+    pub port: u32,
+    pub base_uri: String,
+    pub path_prefix: String,
+
+    pub username: String,
+    pub password: String,
+
+    pub virtual_host: String
+}
+
+fn list_nodes_command(general_args: &ArgMatches) {
+    let target_hostname = general_args.get_one::<String>("host").unwrap();
+    println!("--host: {}", target_hostname);
+}
+
+fn list_users_command(general_args: &ArgMatches) {
+    let target_hostname = general_args.get_one::<String>("host").unwrap();
+    println!("--host: {}", target_hostname);
 }
