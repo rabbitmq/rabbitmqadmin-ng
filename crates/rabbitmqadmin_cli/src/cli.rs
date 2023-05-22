0ec4e9c63846520ae4a819a3ac801adaf845e3cd
Michael Klishin <michael@clojurewerkz.org>
2023-05-22 14:49:20 +0400

    Refactoring

--- crates/rabbitmqadmin_cli/src/cli.rs
+++ crates/rabbitmqadmin_cli/src/cli.rs
@@ -1,5 +1,70 @@
 use super::constants::*;
-use clap::{Arg, Command};
+use clap::{Arg, Command, ArgMatches};
+use url::Url;
+
+#[derive(Debug, Clone)]
+pub struct SharedFlags {
+    pub hostname: String,
+    pub port: u16,
+    pub path_prefix: String,
+
+    pub username: String,
+    pub password: String,
+
+    pub virtual_host: String
+}
+
+impl SharedFlags {
+    pub fn from(general_args: &ArgMatches) -> SharedFlags {
+        if let Some(base_uri) = general_args.get_one::<String>("base_uri") {
+            let url = Url::parse(&base_uri).unwrap();
+            SharedFlags::new_from_uri(&url, &general_args)
+        } else {
+            SharedFlags::new(general_args)
+        }
+    }
+
+    pub fn new(cli_args: &ArgMatches) -> Self {
+        let default_hostname = DEFAULT_HOST.to_owned();
+        let hostname = cli_args.get_one::<String>("host").unwrap_or(&default_hostname);
+        let port = cli_args.get_one::<u16>("port").unwrap();
+        let default_path_prefix = DEFAULT_PATH_PREFIX.to_owned();
+        let path_prefix = cli_args.get_one::<String>("path_prefix").unwrap_or(&default_path_prefix);
+        let username = cli_args.get_one::<String>("username").unwrap();
+        let password = cli_args.get_one::<String>("password").unwrap();
+        let default_vhost = DEFAULT_VHOST.to_owned();
+        let vhost = cli_args.get_one::<String>("vhost").unwrap_or(&default_vhost);
+
+        Self {
+            hostname: hostname.clone(),
+            port: (*port).clone(),
+            path_prefix: path_prefix.clone(),
+            username: username.clone(),
+            password: password.clone(),
+            virtual_host: vhost.clone()
+        }
+    }
+
+    pub fn new_from_uri(url: &Url, cli_args: &ArgMatches) -> Self {
+        let hostname = url.host_str().unwrap_or(&DEFAULT_HOST).to_owned();
+        let port = url.port().unwrap_or(DEFAULT_HTTP_PORT);
+        let default_path_prefix = DEFAULT_PATH_PREFIX.to_owned();
+        let path_prefix = cli_args.get_one::<String>("path_prefix").unwrap_or(&default_path_prefix);
+        let username = cli_args.get_one::<String>("username").unwrap();
+        let password = cli_args.get_one::<String>("password").unwrap();
+        let default_vhost = DEFAULT_VHOST.to_owned();
+        let vhost = cli_args.get_one::<String>("vhost").unwrap_or(&default_vhost);
+
+        Self {
+            hostname: hostname.clone(),
+            port: port.clone(),
+            path_prefix: path_prefix.clone(),
+            username: username.clone(),
+            password: password.clone(),
+            virtual_host: vhost.clone()
+        }
+    }
+}
 
 pub fn parser() -> Command {
     Command::new("rabbitmqadmin")
--- crates/rabbitmqadmin_cli/src/main.rs
+++ crates/rabbitmqadmin_cli/src/main.rs
@@ -2,9 +2,8 @@ mod constants;
 mod cli;
 mod commands;
 
-use constants::*;
+use cli::SharedFlags;
 use clap::{ArgMatches};
-use url::Url;
 
 fn main() {
     let parser = cli::parser();
@@ -26,78 +25,14 @@ fn main() {
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct SharedFlags {
-    pub hostname: String,
-    pub port: u16,
-    pub path_prefix: String,
-
-    pub username: String,
-    pub password: String,
-
-    pub virtual_host: String
-}
-
-impl SharedFlags {
-    pub fn new(cli_args: &ArgMatches) -> Self {
-        let default_hostname = DEFAULT_HOST.to_owned();
-        let hostname = cli_args.get_one::<String>("host").unwrap_or(&default_hostname);
-        let port = cli_args.get_one::<u16>("port").unwrap();
-        let default_path_prefix = DEFAULT_PATH_PREFIX.to_owned();
-        let path_prefix = cli_args.get_one::<String>("path_prefix").unwrap_or(&default_path_prefix);
-        let username = cli_args.get_one::<String>("username").unwrap();
-        let password = cli_args.get_one::<String>("password").unwrap();
-        let default_vhost = DEFAULT_VHOST.to_owned();
-        let vhost = cli_args.get_one::<String>("vhost").unwrap_or(&default_vhost);
-
-        Self {
-            hostname: hostname.clone(),
-            port: (*port).clone(),
-            path_prefix: path_prefix.clone(),
-            username: username.clone(),
-            password: password.clone(),
-            virtual_host: vhost.clone()
-        }
-    }
-
-    pub fn new_from_uri(url: &Url, cli_args: &ArgMatches) -> Self {
-        let hostname = url.host_str().unwrap_or(&DEFAULT_HOST).to_owned();
-        let port = url.port().unwrap_or(DEFAULT_HTTP_PORT);
-        let default_path_prefix = DEFAULT_PATH_PREFIX.to_owned();
-        let path_prefix = cli_args.get_one::<String>("path_prefix").unwrap_or(&default_path_prefix);
-        let username = cli_args.get_one::<String>("username").unwrap();
-        let password = cli_args.get_one::<String>("password").unwrap();
-        let default_vhost = DEFAULT_VHOST.to_owned();
-        let vhost = cli_args.get_one::<String>("vhost").unwrap_or(&default_vhost);
-
-        Self {
-            hostname: hostname.clone(),
-            port: port.clone(),
-            path_prefix: path_prefix.clone(),
-            username: username.clone(),
-            password: password.clone(),
-            virtual_host: vhost.clone()
-        }
-    }
-}
-
-fn shared_flags_from(general_args: &ArgMatches) -> SharedFlags {
-    if let Some(base_uri) = general_args.get_one::<String>("base_uri") {
-        let url = Url::parse(&base_uri).unwrap();
-        SharedFlags::new_from_uri(&url, &general_args)
-    } else {
-        SharedFlags::new(general_args)
-    }
-}
-
 fn list_nodes_command(general_args: &ArgMatches) {
-    let shared: SharedFlags = shared_flags_from(general_args);
+    let shared = SharedFlags::from(general_args);
     println!("list_nodes shared: {:?}", shared);
 
 
 }
 
 fn list_users_command(general_args: &ArgMatches) {
-    let shared: SharedFlags = shared_flags_from(general_args);
+    let shared = SharedFlags::from(general_args);
     println!("list_users shared: {:?}", shared)
 }
