ffeedb5270cc1334af85879de476b002f4e54a92
Michael Klishin <michael@clojurewerkz.org>
2024-04-30 17:49:43 -0400

    Drop a couple of env-specific tests
    Having good error messages is important. However,
    in some environments these tests fail for no
    fault of the app or its underlying HTTP API
    client.
    
    Getting more mileage on rabbitmqadmin v2 is likely
    the best way to polish the CLI interface
    (which is already much better compared to v1).

--- tests/cli.rs
+++ tests/cli.rs
@@ -28,31 +28,6 @@ fn show_subcommands_with_no_arguments() -> Result<(), Box<dyn std::error::Error>
 }
 
 #[test]
-fn nice_error_cant_resolve_host() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("rabbitmqadmin")?;
-
-    cmd.arg("-H").arg("nosuchhost").arg("list").arg("queues");
-    cmd.assert().failure().stderr(
-        predicate::str::contains("failed to lookup address information")
-            .and(predicate::str::contains("nosuchhost")),
-    );
-
-    Ok(())
-}
-
-#[test]
-fn nice_error_when_connection_refused() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("rabbitmqadmin")?;
-
-    cmd.arg("-P").arg("27651").arg("list").arg("queues");
-    cmd.assert().failure().stderr(
-        predicate::str::contains("Connection refused").and(predicate::str::contains("27651")),
-    );
-
-    Ok(())
-}
-
-#[test]
 fn list_vhosts() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("rabbitmqadmin")?;
 
