ae1db217a3589515f582581e0a4fa615c8883ddd
Michael Klishin <michaelklishin@icloud.com>
2024-12-23 19:05:43 -0500

    cargo clippy --fix

--- src/static_urls.rs
+++ src/static_urls.rs
@@ -12,26 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub(crate) const RABBITMQ_DOC_GUIDES_URL: &'static str = "https://rabbitmq.com/docs/";
-pub(crate) const GITHUB_DISCUSSIONS_URL: &'static str =
+pub(crate) const RABBITMQ_DOC_GUIDES_URL: &str = "https://rabbitmq.com/docs/";
+pub(crate) const GITHUB_DISCUSSIONS_URL: &str =
     "https://github.com/rabbitmq/rabbitmq-server/discussions";
-pub(crate) const DISCORD_SERVER_INVITATION_URL: &'static str = "https://rabbitmq.com/discord/";
+pub(crate) const DISCORD_SERVER_INVITATION_URL: &str = "https://rabbitmq.com/discord/";
 
-pub(crate) const GITHUB_REPOSITORY_URL: &'static str =
+pub(crate) const GITHUB_REPOSITORY_URL: &str =
     "https://github.com/rabbitmq/rabbitmqadmin-ng";
 
-pub(crate) const QUORUM_QUEUE_GUIDE_URL: &'static str = "https://rabbitmq.com/docs/quorum-queues";
-pub(crate) const QUORUM_QUEUE_FAILURE_HANDLING_GUIDE_URL: &'static str =
+pub(crate) const QUORUM_QUEUE_GUIDE_URL: &str = "https://rabbitmq.com/docs/quorum-queues";
+pub(crate) const QUORUM_QUEUE_FAILURE_HANDLING_GUIDE_URL: &str =
     "https://www.rabbitmq.com/docs/quorum-queues#leader-election";
-pub(crate) const UPGRADE_GUIDE_URL: &'static str = "https://rabbitmq.com/docs/upgrade";
+pub(crate) const UPGRADE_GUIDE_URL: &str = "https://rabbitmq.com/docs/upgrade";
 #[allow(unused)]
-pub(crate) const BLUE_GREEN_UPGRADE_GUIDE_URL: &'static str =
+pub(crate) const BLUE_GREEN_UPGRADE_GUIDE_URL: &str =
     "https://www.rabbitmq.com/docs/blue-green-upgrade";
-pub(crate) const MONITORING_GUIDE_URL: &'static str = "https://rabbitmq.com/docs/monitoring";
-pub(crate) const HEALTH_CHECK_GUIDE_URL: &'static str =
+pub(crate) const MONITORING_GUIDE_URL: &str = "https://rabbitmq.com/docs/monitoring";
+pub(crate) const HEALTH_CHECK_GUIDE_URL: &str =
     "https://rabbitmq.com/docs/monitoring#health-checks";
-pub(crate) const DEPRECATED_FEATURE_GUIDE_URL: &'static str =
+pub(crate) const DEPRECATED_FEATURE_GUIDE_URL: &str =
     "https://rabbitmq.com/docs/deprecated-features";
-pub(crate) const HTTP_API_ACCESS_PERMISSIONS_GUIDE_URL: &'static str =
+pub(crate) const HTTP_API_ACCESS_PERMISSIONS_GUIDE_URL: &str =
     "https://rabbitmq.com/docs/management#permissions";
-pub(crate) const DEFINITION_GUIDE_URL: &'static str = "https://rabbitmq.com/docs/definitions";
+pub(crate) const DEFINITION_GUIDE_URL: &str = "https://rabbitmq.com/docs/definitions";
